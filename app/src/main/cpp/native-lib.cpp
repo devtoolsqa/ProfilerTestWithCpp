c1bc0ea68c1cfed048771011897f23102d53be5d
sandeeppadala <spadala88@gmail.com>
2020-03-27 11:51:31 +0530

    test commit

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,6 @@ extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_cppdemo_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++ sandeep";
+    std::string hello = "Hello from C++ sandeep padala";
     return env->NewStringUTF(hello.c_str());
 }
